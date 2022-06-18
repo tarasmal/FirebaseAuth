1de04575c6e62a01646a1514d95f31fed3970f25
pogromister <tarasmalarcuk8@gmail.com>
2022-06-18 23:50:32 +0300

    v1.0

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {auth} from "../firebase";
 import {useNavigate} from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 const HomePage = () => {
     const [name, setName] = useState('')
     const navigate = useNavigate()
@@ -15,23 +15,24 @@ const HomePage = () => {
             }
         })
     }, [])
+    const out = async () => {
+        try{
+            await signOut(auth)
+        }
+        catch (e){
+            console.log(e.message)
+        }
+    }
     return (
         <>
             <nav className={'navbar navbar-expand-lg navbar-light bg-light'}>
                 <ul className={'navbar-nav me-auto mb-2 mb-lg-0'}>
-                    <div className={'col'}></div>
-                    <div className={'col'}></div>
-                    <div className={'col'}></div>
-                    <div className={'col'}></div>
-                    <div className={'col'}></div>
-                    <div className={'col'}></div>
 
-                    <div className={'col'}></div>
-                    <div className={'col'}></div>
-                    <div className={'col'}></div>
-
-                    <li className={'nav-item m-2 float-right'}>
+                    <li className={'nav-item m-2'}>
                         {name}
+                    </li>
+                    <li className={'nav-item m-2'}>
+                        <button className={'btn-secondary'} onClick={out}>Sign out</button>
                     </li>
                 </ul>
             </nav>
